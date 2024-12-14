541e7dd405e077d699e856177729f945ced511b3
Kun Chen <chen.kun.tw@gmail.com>
2024-12-14 11:47:36 +0800

    Add files via upload

---
+++ Practice_11_1.py
@@ -0,0 +1,169 @@
+import torch
+import torch.nn as nn
+import pandas as pd
+import numpy as np
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+from torch import autograd
+from torch.autograd import Variable
+from torchvision.utils import make_grid
+import matplotlib.pyplot as plt
+from torchvision.datasets import FashionMNIST
+PATH_DATASETS = "" # Default path
+BATCH_SIZE = 64 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+transform=transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,)),
+])
+# dataset = FashionMNIST('', train=True, download=True, transform=transform)
+# data_loader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+dataset = MNIST(PATH_DATASETS, train=True, download=True, 
+                 transform=transform)
+data_loader = torch.utils.data.DataLoader(dataset
+                   , batch_size=BATCH_SIZE, shuffle=True)
+
+print(dataset[0][0])
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        # Sets the embedding layer as input to Label
+        self.label_emb = nn.Embedding(10, 10)
+        
+        self.model = nn.Sequential(
+            nn.Linear(110, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 1024),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(1024, 784),
+            nn.Tanh()
+        )
+    
+    def forward(self, z, labels):
+        z = z.view(z.size(0), 100) 
+        c = self.label_emb(labels) 
+        x = torch.cat([z, c], 1)  # Merge inputs
+        out = self.model(x)
+        return out.view(x.size(0), 28, 28)
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        # Sets the embedding layer as input to Label
+        self.label_emb = nn.Embedding(10, 10)
+        
+        self.model = nn.Sequential(
+            nn.Linear(794, 1024),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.3),
+            nn.Linear(1024, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.3),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.3),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+    
+    def forward(self, x, labels):
+        x = x.view(x.size(0), 784)
+        c = self.label_emb(labels)
+        x = torch.cat([x, c], 1)  # Merge inputs
+        out = self.model(x)
+        return out.squeeze()
+    
+generator = Generator().to(device)
+discriminator = Discriminator().to(device)
+criterion = nn.BCELoss()
+d_optimizer = torch.optim.Adam(discriminator.parameters(), lr=1e-4)
+g_optimizer = torch.optim.Adam(generator.parameters(), lr=1e-4)
+def generator_train_step(batch_size, discriminator, generator, 
+                         g_optimizer, criterion):
+    g_optimizer.zero_grad()
+    z = Variable(torch.randn(batch_size, 100)).to(device)
+    fake_labels = Variable(torch.LongTensor(np.random.randint(0, 10, 
+                              batch_size))).to(device)# Random random numbers [1, 10]
+    fake_images = generator(z, fake_labels)
+    validity = discriminator(fake_images, fake_labels)
+    g_loss = criterion(validity, Variable(torch.ones(batch_size)).to(device))
+    g_loss.backward()
+    g_optimizer.step()
+    return g_loss.data.item()
+
+def discriminator_train_step(batch_size, discriminator, generator
+                         , d_optimizer, criterion, real_images, labels):
+    d_optimizer.zero_grad()
+    # Train for real image
+    real_validity = discriminator(real_images, labels)
+    real_loss = criterion(real_validity, Variable(torch.ones(
+                batch_size)).to(device))
+    
+    # Train for fake image
+    z = Variable(torch.randn(batch_size, 100)).to(device)
+    fake_labels = Variable(torch.LongTensor(np.random.randint(
+                0, 10, batch_size))).to(device)  # Random random numbers [1, 10]
+    fake_images = generator(z, fake_labels)
+    fake_validity = discriminator(fake_images, fake_labels)
+    fake_loss = criterion(fake_validity, Variable(torch.zeros(
+                batch_size)).to(device))
+    
+    d_loss = real_loss + fake_loss
+    d_loss.backward()
+    d_optimizer.step()
+    return d_loss.data.item()
+
+
+num_epochs = 30
+n_critic = 5
+display_step = 300
+for epoch in range(num_epochs):
+    print('Starting epoch {}...'.format(epoch))
+    for i, (images, labels) in enumerate(data_loader):
+        real_images = Variable(images).to(device)
+        labels = Variable(labels).to(device)
+        generator.train()
+        batch_size = real_images.size(0)
+        d_loss = discriminator_train_step(len(real_images), discriminator,
+                                          generator, d_optimizer, criterion,
+                                          real_images, labels)
+        g_loss = generator_train_step(batch_size, discriminator, 
+                                      generator, g_optimizer, criterion)
+    generator.eval()
+    print('g_loss: {}, d_loss: {}'.format(g_loss, d_loss))
+    z = Variable(torch.randn(9, 100)).to(device)
+    labels = Variable(torch.LongTensor(np.arange(9))).to(device)
+    sample_images = generator(z, labels).unsqueeze(1).data.cpu()
+    grid = make_grid(sample_images, nrow=3, normalize=True)\
+                .permute(1,2,0).numpy()
+    plt.imshow(grid)
+    plt.axis('off')
+    plt.show()
+
+# Label the name
+label_names = ['T-Shirt', 'Trouser', 'Pullover', 'Dress', 'Coat'
+               , 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+z = Variable(torch.randn(100, 100)).to(device)
+labels = Variable(torch.LongTensor([i for _ in range(10) for i 
+                                    in range(10)])).to(device)
+# Generate images
+sample_images = generator(z, labels).unsqueeze(1).data.cpu()
+grid = make_grid(sample_images, nrow=10, normalize=True)\
+                            .permute(1,2,0).numpy()
+# Display the image
+fig, ax = plt.subplots(figsize=(15,15))
+ax.imshow(grid)
+plt.yticks([])
+plt.xticks(np.arange(15, 300, 30), label_names, 
+           rotation=45, fontsize=20);
+
